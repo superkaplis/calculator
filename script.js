be77ca26f836b720d75c8142f1101fb08ae3c1e4
superkaplis <dzintarsmartins@gmail.com>
2022-08-26 17:13:57 +0300

    added hover effects

--- script.js
+++ script.js
@@ -1,38 +1,81 @@
+let resultText = document.querySelector('#result-text');
+let displayValue = '';
+let firstNumber;
+let latestOperator;
+
 function add(a, b) {
-  console.log(Number(a) + Number(b));
+  let x = Number(a) + Number(b);
+  displayValue = x;
+  resultText.textContent = displayValue;
 }
 
 
 function subtract(a, b) {
-  console.log(Number(a) - Number(b));
+  let x = Number(a) - Number(b);
+  displayValue = x;
+  resultText.textContent = displayValue;
 }
 
 function multiply(a, b) {
-  console.log(Number(a) * Number(b));
+  let x = Number(a) * Number(b);
+  displayValue = Math.round(x * 10) / 10;
+  resultText.textContent = displayValue;
 }
 
 function divide(a, b) {
-  let rounded = Math.round((a / b)* 10) / 10  
-  console.log(rounded);
+  let x = Math.round((a / b) * 10) / 10;
+  displayValue = x;
+  resultText.textContent = displayValue;
 }
 
-function operate(){
-  let a = prompt("Enter first number: ");
-  let op = prompt("Enter operation (+, -, *, /) ");
-  let b = prompt("Enter second number: ");
-
-  if(op == '+') add(a, b);
-  if(op == '-') subtract(a, b);
-  if(op == '*') multiply(a, b);
-  if(op == '/') divide(a, b);
+function operate(a, b, op) {
+  if (op == '+') add(a, b);
+  if (op == '-') subtract(a, b);
+  if (op == '*') multiply(a, b);
+  if (op == '/') divide(a, b);
 }
-// CONSOLE FUNCTION UP
 
-let resultText = document.querySelector('#result-text');
-let displayValue = '';
-
-function numClick(btsn){
-  let cont = btsn.textContent;
+function numClick(btn) {
+  let cont = btn.textContent;
   displayValue += cont;
   resultText.textContent = displayValue;
 }
+
+function operatorClick(operator) {
+  let cont = operator.textContent;
+  firstNumber = Number(displayValue);
+  latestOperator = cont;
+  displayValue = cont;
+  resultText.textContent = displayValue;
+  displayValue = '';
+}
+
+function resultClick() {
+  console.log(firstNumber)
+  console.log(latestOperator)
+  console.log(displayValue)
+  console.log('result: ')
+
+  let first = firstNumber;
+  let oper = latestOperator;
+  let last = Number(displayValue);
+  operate(first, last, oper);
+}
+
+function clearAll() {
+  resultText.textContent = '0';
+  displayValue = '';
+  firstNumber = undefined;
+  latestOperator = undefined;
+}
+
+let buttons = document.querySelectorAll('button');
+
+for (let elem of buttons) {
+  elem.addEventListener('mouseenter', () => {
+    elem.style.backgroundColor = 'aqua'
+  })
+  elem.addEventListener('mouseleave', () => {
+    elem.style.backgroundColor = ''
+  })
+};
